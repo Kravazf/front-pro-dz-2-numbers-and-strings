8d69b2aa2e11f567802d42b20786c4f8a44f437c
Maks Kravchenko <krava.zf@gmail.com>
2023-04-24 20:36:56 +0300

    7th-line-in-the-template-sentence

--- markap/index.js
+++ markap/index.js
@@ -4,5 +4,5 @@ city = prompt('In which city do you live?');
 console.log(`${lastName} ${firstName} ${city}`);
 
 number = prompt('enter a five-digit number');
-breakdownByIndex = number[0] + ' ' + number[1] + ' ' + number[2] + ' ' + number[3] + ' ' + number[4];
+breakdownByIndex = `${number[0]} ${number[1]} ${number[2]} ${number[3]} ${number[4]}`;
 alert(breakdownByIndex);
