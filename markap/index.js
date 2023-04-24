90b4dd2defa303e19601dfd06827c75247b0397d
Maks Kravchenko <krava.zf@gmail.com>
2023-04-24 16:43:42 +0300

    dz-3-mathematical-operations-done

---
+++ markap/index.js
@@ -0,0 +1,8 @@
+firstName = prompt('Whut is your name?');
+lastName = prompt('What is your last name?');
+city = prompt('In which city do you live?');
+console.log(`${lastName} ${firstName} ${city}`);
+
+number = prompt('enter a five-digit number');
+breakdownByIndex = number[0] + ' ' + number[1] + ' ' + number[2] + ' ' + number[3] + ' ' + number[4];
+alert(breakdownByIndex);
